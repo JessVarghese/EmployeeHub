0b12aa218994bab84ab011a3e605971d71df44ec
Jess Varghese <jessvarghese@gmail.com>
2021-12-16 13:53:58 -0500

    created the employees table

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,6 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_is INTEGER
+  manager_id INTEGER
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -24,3 +24,19 @@ VALUES
 ('producer', 85000, 3),
 ('marketing lead', 90000, 1);
 
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Kelly', 'Arnold', 1, '0524'),
+('Lisa', 'Crumb', 2, '8090'),
+('Becky', 'Ham', 3, '7345'),
+('Nicole', 'Spenz', 1, '0524'),
+('Bronwyn', 'Beltz', 2, '8090'),
+('Rachelle', 'Lopez', 3, '7345'),
+('Ophelia', 'Lauren', 1, '0524'),
+('Cathy', 'Candle', 2, '8090'),
+('Simone', 'Cloud', 3, '7345'),
+('Tinsley', 'Montgomery', 1, '0524');
+
+
+
