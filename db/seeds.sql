2b28c6adcedb40f4b8c0f57638f82da547793283
Jess Varghese <jessvarghese@gmail.com>
2021-12-19 11:57:08 -0500

    updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -28,14 +28,14 @@ VALUES
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Kelly', 'Arnold', 1, 1),
-('Lisa', 'Crumb', 2, 1),
-('Becky', 'Ham', 3, 1),
+('Lisa', 'Crumb', 2, 2),
+('Becky', 'Ham', 3, 3),
 ('Nicole', 'Spenz', 1, 1),
-('Bronwyn', 'Beltz', 2, 1),
-('Rachelle', 'Lopez', 3, 1),
+('Bronwyn', 'Beltz', 2, 2),
+('Rachelle', 'Lopez', 3, 3),
 ('Ophelia', 'Lauren', 1, 1),
-('Cathy', 'Candle', 2, 1),
-('Simone', 'Cloud', 3, 1),
+('Cathy', 'Candle', 2, 2),
+('Simone', 'Cloud', 3, 3),
 ('Tinsley', 'Montgomery', 1, 1);
 
 
